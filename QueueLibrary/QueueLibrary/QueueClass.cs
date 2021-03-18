897c433d8c93144801931be3414fba5bb5cc448d
Mike <Nortonstudios@gmail.com>
2021-03-17 18:46:00 -0600

    Initial commit. Grouping all data structures projects into one folder. Initial work on Queue.

---
+++ QueueLibrary/QueueLibrary/QueueClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace QueueLibrary
+{
+    public class QueueClass
+    {
+        //Props and fields
+        public QueueNode Front { get; private set; }
+        public QueueNode Back { get; private set; }
+
+        //public methods
+        public void enqueue(object payload)
+        {
+
+            Back = new QueueNode(payload, Back);
+            throw new NotImplementedException();
+            //Front and Back need to be the same node in a 1 node queue.
+            //Eliminate one or the other?
+            //
+
+
+        }
+
+        public object dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            if (Front == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public QueueClass()
+        {
+        }
+    }
+}
---
+++ QueueLibrary/QueueLibrary/QueueNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace QueueLibrary
+{
+    public class QueueNode
+    {
+        //Props and fields
+        public object Payload { get; private set; }
+        public QueueNode Next { get; private set; }
+
+        //Constructor
+        public QueueNode(object payload, QueueNode next)
+        {
+            Payload = payload;
+            Next = next;
+        }
+    }
+}
